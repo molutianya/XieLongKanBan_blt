47234e76482517d83826d2c2005a5ba69be76dea
lele <lele112233@126.com>
2020-03-29 23:27:35 +0800

    升级

--- app/src/main/java/com/example/a86156/lunbo_2/App.java
+++ app/src/main/java/com/example/a86156/lunbo_2/App.java
@@ -2,8 +2,7 @@ package com.example.a86156.lunbo_2;
 
 public class App {
 
-    public static String url = "http://192.168.32.201:6081/meswebservice.asmx/"; //协隆服务器ip地址
-   // public static String url = "http://119.29.103.79:6091/meswebservice.asmx/"; //服务器ip地址
+    public static String url = ""; //协隆服务器ip地址
     public static String GetSertime = "GetSertime"; //返回服务器时间
     public static String KanbanYFAll = "KanbanYFAll?"; //返回表头数据
     public static String MoBLQtyPer = "MoBLQtyPer?"; //返回柏拉图数据
